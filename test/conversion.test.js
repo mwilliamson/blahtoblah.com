512f55dc2a83156e746d4c33d54692d7c0ac0b49
Michael Williamson <mike@zwobble.org>
2012-01-15 15:50:59 +0000

    Remove some duplication in tests

--- test/conversion.test.js
+++ test/conversion.test.js
@@ -1,102 +1,63 @@
 var convert = require("../lib/conversion.js");
 
+var converter = convert.createConverter({
+    units: [
+        {names: ["centimetres"], size: 0.01, dimension: "length"},
+        {names: ["metres"], size: 1, dimension: "length"},
+    ]
+});
+
 exports.noOutputIfFromIsEmpty = function(test) {
-    var converter = convert.createConverter();
     test.equal(converter("", "inches"), null);
     test.done();
 };
 
 exports.noOutputIfToIsEmpty = function(test) {
-    var converter = convert.createConverter();
     test.equal(converter("4 metres", ""), null);
     test.done();
 };
 
 exports.noOutputIfFromIsWhitespace = function(test) {
-    var converter = convert.createConverter();
     test.equal(converter("  ", "inches"), null);
     test.done();
 };
 
 exports.noOutputIfToIsWhitespace = function(test) {
-    var converter = convert.createConverter();
     test.equal(converter("4 metres", "  "), null);
     test.done();
 };
 
 exports.convertsBetweenUnitsIfTheUnitsRepresentTheSameMetric = function(test) {
-    var converter = convert.createConverter({
-        units: [
-            {names: ["centimetres"], size: 0.01, siUnit: "m"},
-            {names: ["metres"], size: 1, siUnit: "m"},
-        ]
-    });
     test.equal(converter("4 metres", "centimetres"), "4 metres is 400 centimetres");
     test.done();
 };
 
 exports.inputStringsAreCleanedUp = function(test) {
-    var converter = convert.createConverter({
-        units: [
-            {names: ["centimetres"], size: 0.01, siUnit: "m"},
-            {names: ["metres"], size: 1, siUnit: "m"},
-        ]
-    });
     test.equal(converter("  4    metres", " centimetres "), "4 metres is 400 centimetres");
     test.done();
 };
 
 exports.inputStringsAreCleanedUp = function(test) {
-    var converter = convert.createConverter({
-        units: [
-            {names: ["centimetres"], size: 0.01, siUnit: "m"},
-            {names: ["metres"], size: 1, siUnit: "m"},
-        ]
-    });
     test.equal(converter("  4    metres", " centimetres "), "4 metres is 400 centimetres");
     test.done();
 };
 
 exports.errorMessageIfFromStringIsInWrongFormat = function(test) {
-    var converter = convert.createConverter({
-        units: [
-            {names: ["centimetres"], size: 0.01, siUnit: "m"},
-            {names: ["metres"], size: 1, siUnit: "m"},
-        ]
-    });
     test.equal(converter("a", "centimetres"), "a doesn't look like a quantity to me");
     test.done();
 };
 
 exports.errorMessageIfFromSizeIsUnreadable = function(test) {
-    var converter = convert.createConverter({
-        units: [
-            {names: ["centimetres"], size: 0.01, siUnit: "m"},
-            {names: ["metres"], size: 1, siUnit: "m"},
-        ]
-    });
     test.equal(converter("sdf metres", "centimetres"), "sdf doesn't look like a number to me");
     test.done();
 };
 
 exports.errorMessageIfFromUnitsIsNotRecognised = function(test) {
-    var converter = convert.createConverter({
-        units: [
-            {names: ["centimetres"], size: 0.01, siUnit: "m"},
-            {names: ["metres"], size: 1, siUnit: "m"},
-        ]
-    });
     test.equal(converter("8 inches", "centimetres"), "I don't recognise \"inches\"");
     test.done();
 };
 
 exports.errorMessageIfToUnitsIsNotRecognised = function(test) {
-    var converter = convert.createConverter({
-        units: [
-            {names: ["centimetres"], size: 0.01, siUnit: "m"},
-            {names: ["metres"], size: 1, siUnit: "m"},
-        ]
-    });
     test.equal(converter("8 metres", "inches"), "I don't recognise \"inches\"");
     test.done();
 };
