502f3f6a6586d5785b2976f89d6e0debddea3796
Michael Williamson <mike@zwobble.org>
2012-01-15 11:31:35 +0000

    Handle inputs in the wrong format

--- test/conversion.test.js
+++ test/conversion.test.js
@@ -33,3 +33,25 @@ exports.inputStringsAreCleanedUp = function(test) {
     test.equal(converter("  4    metres", " centimetres "), "4 metres is 400 centimetres");
     test.done();
 };
+
+exports.inputStringsAreCleanedUp = function(test) {
+    var converter = convert.createConverter({
+        units: [
+            {names: ["centimetres"], size: 0.01, siUnit: "m"},
+            {names: ["metres"], size: 1, siUnit: "m"},
+        ]
+    });
+    test.equal(converter("  4    metres", " centimetres "), "4 metres is 400 centimetres");
+    test.done();
+};
+
+exports.errorMessageIfCannotParseFrom = function(test) {
+    var converter = convert.createConverter({
+        units: [
+            {names: ["centimetres"], size: 0.01, siUnit: "m"},
+            {names: ["metres"], size: 1, siUnit: "m"},
+        ]
+    });
+    test.equal(converter(" a  ", " centimetres "), "a doesn't look like a quantity to me");
+    test.done();
+};
